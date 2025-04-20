65e27d215413ce1fe556ffd8e392c689b5a900c4
Cesar Sanchez <cesar.sanchezibr@gmail.com>
2025-04-20 21:24:42 +0200

    fix(env): update name of keys for cloud provider

--- lib/services/storage.ts
+++ lib/services/storage.ts
@@ -12,10 +12,10 @@ export class Storage {
 
   constructor() {
     this.s3Client = new S3Client({
-      region: process.env.AWS_REGION || 'us-east-1',
+      region: process.env.AWS_DEFAULT_REGION || 'us-east-2',
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+        accessKeyId: process.env.AWS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_ACCESS_KEY || '',
       },
     });
     
